cddb2c19fe4044a6e244159d69a19ee5b1e62340
Jay <49783876+JCProjector@users.noreply.github.com>
2023-04-28 13:12:06 +0100

    Generate base project

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:victrix/main.dart';
+
+void main() {
+  testWidgets('', (WidgetTester tester) async {});
+}
